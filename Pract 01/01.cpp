d92646fa8d2d261ffa21e4cb2bed610865017c39
Deniz Shyukryuev <72249306+denizdsh@users.noreply.github.com>
2024-10-07 02:19:59 +0300

    Add files via upload

---
+++ Pract 01/01.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+int main()
+{
+	//0
+	std::cout << "Hello, FMI"
+		<< std::endl << std::endl;
+
+	//1
+	double triangleA, triangleB, triangleC;
+	std::cin >> triangleA >> triangleB >> triangleC;
+
+	if (triangleA + triangleB <= triangleC
+		|| triangleA + triangleC <= triangleB
+		|| triangleB + triangleC <= triangleA)
+	{
+		throw std::invalid_argument("Invalid triangle");
+	}
+
+	std::cout << triangleA + triangleB + triangleC
+		<< std::endl << std::endl;
+
+	//2
+	double celsius;
+	std::cin >> celsius;
+	std::cout << "Kelvin: " << celsius + 273.15 << "\nFahrenheit: " << celsius * 9 / 5 + 32 << std::endl << std::endl;
+
+	//3
+	std::string a, b, temp;
+	std::cin >> a >> b;
+
+	temp = a;
+	a = b;
+	b = temp;
+
+	std::cout << "a: " << a << ", b: " << b
+		<< std::endl << std::endl;
+
+	//4
+	int totalSec, days, hours, minutes, seconds;
+	std::cin >> totalSec;
+
+	seconds = totalSec % 60;
+	minutes = totalSec % (60 * 60) / 60;
+	hours = (totalSec / (60 * 60)) % 24;
+	days = (totalSec / (60 * 60)) / 24;
+
+	std::cout << days << " days, "
+		<< hours << " hours, "
+		<< minutes << " minutes, "
+		<< seconds << " seconds"
+		<< std::endl << std::endl;
+
+	//5
+	int dividend, divisor;
+	std::cin >> dividend >> divisor;
+
+	if (divisor == 0)
+	{
+		throw std::invalid_argument("Cannot divide by zero");
+	}
+
+	std::cout << std::boolalpha << ((dividend % divisor) == 0)
+		<< std::endl << std::endl;
+
+	//6
+	//6. Method 1
+	std::string code;
+	std::cin >> code;
+
+	code.replace(0, 7, "*******");
+
+	std::cout << code;
+
+	/*
+	6. Method 2
+	unsigned long long code;
+	std::cin >> code;
+
+	std::cout << "*******" << code % 1000;
+	*/
+
+	std::cout << std::endl;
+}
+
+
