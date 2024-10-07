30083a13c78a4fe8fee131a81e9e3c94c2e1de6b
Hristo <114760077+hripteR@users.noreply.github.com>
2024-10-07 14:56:46 +0300

    Create Task01.cpp

---
+++ Task01.cpp
@@ -0,0 +1,27 @@
+// zad 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    double a = 10;
+    double b = 7;
+    double c = 6.5;
+    double P = a + b + c;
+    double S = sqrt((P / 2) * (P / 2 - a) * (P / 2 - b) * (P / 2 - c));
+    std::cout << "P = " << P;
+    std::cout << "\nS = " << S;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
