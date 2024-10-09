24f24bd7dc85529be8b2ea18ab8be0d7fbe29991
olgica9m <147002285+olgica9m@users.noreply.github.com>
2024-10-09 22:18:47 +0300

    Add files via upload

---
+++ Pract02-Taskk06.cpp
@@ -0,0 +1,48 @@
+// ConsoleApplication267.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int cheeseburgers, fishMenus, chickenWings;
+
+    
+    const double priceCheeseburger = 8.25;
+    const double priceFishMenu = 12.00;
+    const double priceChickenWings = 10.99;
+    const double deliveryCharge = 5.00;
+
+    cin >> cheeseburgers >> fishMenus >> chickenWings;
+
+    double totalBill = (cheeseburgers * priceCheeseburger) +
+        (fishMenus * priceFishMenu) +
+        (chickenWings * priceChickenWings);
+
+    
+    double dessert = (totalBill > 60) * (totalBill * 0.10);
+
+    
+    totalBill += dessert + deliveryCharge;
+
+    
+    totalBill = int(totalBill * 100 + 0.5) / 100.0;
+
+   
+    cout << totalBill << endl;
+
+    return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
