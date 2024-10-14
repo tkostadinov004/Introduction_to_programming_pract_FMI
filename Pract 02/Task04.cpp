9b66c0b7488471b3b143cd64047ff24197309ed7
Nikol Georgieva <nicole.georgieva18@abv.bg>
2024-10-15 00:31:39 +0300

    Update Task04.cpp

--- Pract 02/Task04.cpp
+++ Pract 02/Task04.cpp
@@ -6,7 +6,7 @@ int main()
 
 	std::cin >> a >> b >> c >> d;
 
-	bool areCrossing = (b >= c && b <= d) || (a >= c && a <= d);
+	bool areCrossing = (b >= c) && (a <= d);
 
 	std::cout << areCrossing;
 }
