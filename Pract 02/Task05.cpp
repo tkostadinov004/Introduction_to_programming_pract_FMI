9dcb6549a25a25fef4e70e7f3e9e6965dd1077ad
Viki <91424361+viki441@users.noreply.github.com>
2024-10-15 16:44:45 +0100

    Update Task05.cpp

--- Pract 02/Task05.cpp
+++ Pract 02/Task05.cpp
@@ -10,8 +10,13 @@ int main()
     //let's slice the number and reverse the first half
     short firstHalf = (number % 10) * 10 + (number % 100) / 10;
     short secondHalf = (number - number % 100) / 100;
-   /*in task 05 there is no rule not to use the ternary 
+    /*in task 05 there is no rule not to use the ternary 
     operator or conditional operator*/
-    std::cout << (firstHalf == secondHalf) ? 0 : 1;
+    //substract the numbers;
+    //if they are equal we get 0, representing false
+    //if not, we get number != 0, representing true
+    bool result = firstHalf - secondHalf;
+    //we simply say !result to reverse the true and false
+    std::cout << !result;
 }
 
