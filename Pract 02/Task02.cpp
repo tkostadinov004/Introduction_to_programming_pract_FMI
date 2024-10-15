a37e6cf5c93d04555241f6baeaed80797942f420
Viki <91424361+viki441@users.noreply.github.com>
2024-10-15 16:52:04 +0100

    Update Task02.cpp

--- Pract 02/Task02.cpp
+++ Pract 02/Task02.cpp
@@ -6,8 +6,9 @@ int main()
 	std::cout << "Enter two real and imaginary parts: ";
 	std::cin >> re1 >> im1 >> re2 >> im2;
 
-	double reResult = (re1 * re2 + im1 * im2) / (re2 * re2 + im2 * im2);
-	double imResult = (re2 * im1 - re1 * im2) / (re2 * re2 + im2 * im2);
+	int denominator = re2 * re2 + im2 * im2;
+	double reResult = (re1 * re2 + im1 * im2) / denominator;
+	double imResult = (re2 * im1 - re1 * im2) / denominator;
 	
 	
 		std::cout << reResult << " + (" << imResult << "i)";
