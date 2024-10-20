c4702c7e23d0975bfc2942b4f337761f2c46ee93
Emil Nenkov <enenkov1337@gmail.com>
2024-10-20 23:43:52 +0300

    Create Task07.cpp

---
+++ Pract 03/Task07.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+    int hours, minutes;
+    std::cin >> hours >> minutes;
+
+    minutes += 15;
+
+    if (minutes >= 60) {
+        minutes -= 60;
+        hours++;
+    }
+
+    if (hours == 24) {
+        hours = 0;
+    }
+
+    if (hours < 10) {
+        std::cout << '0';
+    }
+
+    std::cout << hours << ' ';
+
+    if (minutes < 10) {
+        std::cout << '0';
+    }
+
+    std::cout << minutes;
+}
