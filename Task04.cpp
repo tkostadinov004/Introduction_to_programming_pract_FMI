7104ad84a68b12803bde0fbc1dbe368311340a47
Hristo <114760077+hripteR@users.noreply.github.com>
2024-10-21 12:49:34 +0300

    Create Task04.cpp

---
+++ Task04.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int main()
+{
+	int a;
+	std::cin >> a;
+	if (a == 1)
+	{
+		std::cout << "I";
+	}
+	else if (a == 2)
+	{
+		std::cout << "II";
+	}
+	else if (a == 3)
+	{
+		std::cout << "III";
+	}
+	else if (a == 4)
+	{
+		std::cout << "IV";
+	}
+
+	else if (a == 5)
+	{
+		std::cout << "V";
+	}
+	else if (a == 6)
+	{
+		std::cout << "VI";
+	}
+	else if (a == 7)
+	{
+		std::cout << "VII";
+	}
+	else if (a == 8)
+	{
+		std::cout << "VIII";
+	}
+	else if (a == 9)
+	{
+		std::cout << "IX";
+	}
+}
