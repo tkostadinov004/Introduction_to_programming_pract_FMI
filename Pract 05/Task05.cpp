501378dd819c836624b1afd1ad04e5b7ea233a8b
Viki <91424361+viki441@users.noreply.github.com>
2024-11-06 15:20:09 +0000

    Update Task05.cpp

--- Pract 05/Task05.cpp
+++ Pract 05/Task05.cpp
@@ -2,17 +2,16 @@
 int numCheck(){
     int num;
     do {
-        std::cout << "Enter two positive numbers: ";
         std::cin >> num;
     } while (num < 0);
     return num;
 }
 int concatNums(int num1, int num2) {
     int duplNum2 = num2;
-    while (duplNum2 > 0) {
-       num1 *= 10;
-       duplNum2 /= 10; 
-    }
+    do {
+        num1 *= 10;
+        duplNum2 /= 10;
+    } while (duplNum2 > 0);
     num1 += num2;
     return num1;
 }
