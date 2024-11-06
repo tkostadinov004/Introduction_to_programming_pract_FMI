27c326e0a6c33d55f788a434fd6c5566949eac7a
Emil Nenkov <enenkov1337@gmail.com>
2024-11-06 11:36:46 +0200

    Update Task08.cpp

--- Pract 05/Task08.cpp
+++ Pract 05/Task08.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
+bool isDivisibleBy11(int num, int digit) {
+    return (num / 1000 * 100 + digit) % 11 == 0;
+}
+
 void printRes(int m, int n) {
     int digit;
     for (int i = m; i <= n; i++) {
         digit = i % 100;
-        if ((i / 1000 * 100 + digit) % 11 == 0) {
+        if (isDivisibleBy11(i, digit)) {
             std::cout << i << ' ';
         }
     }
