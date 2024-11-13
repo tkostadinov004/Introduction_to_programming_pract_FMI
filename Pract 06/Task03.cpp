76a3ec5a4abf45dead9d707df0fffc43d1d2f31f
Viki <91424361+viki441@users.noreply.github.com>
2024-11-13 11:22:22 +0000

    Update Task03.cpp

--- Pract 06/Task03.cpp
+++ Pract 06/Task03.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
-void enterArray(int arr[], int n)
+void reverseArray(int arr[], int size, int reversedArr[]) {
+    
+    for (int i = 0; i < size; i++) {
+        reversedArr[i] = arr[size - 1 - i];
+    }
+}
+
+void enterArray(int arr[], int n, int reversedArr[])
 {
     for (int i = 0; i < n; i++)
     {
         std::cin >> arr[i];
     }
+    reverseArray(arr, n, reversedArr);
 }
+
 int main()
 {
     const unsigned int SIZE = 100;
@@ -13,11 +22,12 @@ int main()
     std::cout << "Enter a number <= 100: ";
     std::cin >> n;
     int arr[SIZE];
+    int reversedArr[SIZE];
     
     if (n <= 100) {
-        enterArray(arr, n);
-        for (int i = n - 1; i >= 0; i--) {
-            std::cout << arr[i] << " ";
+        enterArray(arr, n, reversedArr);
+        for (int i = 0; i < n; i++) {
+            std::cout << reversedArr[i] << " ";
         }
     }
     else {
